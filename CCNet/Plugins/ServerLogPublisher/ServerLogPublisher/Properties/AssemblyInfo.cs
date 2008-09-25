5d2ced298aa5a242d3328e790928e61565e3c00a
david@gardiner.net.au <david@gardiner.net.au@0e962e30-8b01-11dd-b073-6769e66693fe>
2008-09-25 13:32:12 +0000

    Initial version

---
+++ CCNet/Plugins/ServerLogPublisher/ServerLogPublisher/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServerLogPublisher")]
+[assembly: AssemblyDescription("Publish CruiseControl.NET's server log to a project's build log")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("David Gardiner")]
+[assembly: AssemblyProduct("Ccnet.Gardiner.ServerLogPublisher.Plugin")]
+[assembly: AssemblyCopyright("Copyright © David Gardiner 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64a9f482-a083-4fc9-927b-64750f5b6837")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: System.CLSCompliant(false)]
