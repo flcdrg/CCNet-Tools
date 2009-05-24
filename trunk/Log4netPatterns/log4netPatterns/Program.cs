c77a7631e42e1f195a5a30717f9bb3bcae009a1b
david@gardiner.net.au <david@gardiner.net.au@0e962e30-8b01-11dd-b073-6769e66693fe>
2009-05-24 06:34:22 +0000

    Explore pattern output from log4net

---
+++ trunk/Log4netPatterns/log4netPatterns/Program.cs
@@ -0,0 +1,54 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace log4netPatterns
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+
+            var t = new Thingy<int>();
+            t.Doover(3);
+
+            var t2 = new Thangy();
+            t2.Doover("blah");
+
+        }
+    }
+
+    public class Thingy<T>
+    {
+        private ILog _log;
+
+        public Thingy()
+        {
+            _log = LogManager.GetLogger(typeof (Thingy<T>));
+        }
+
+        public virtual string Doover(T stuff)
+        {
+            _log.Debug("Thingy");
+
+            return "ha";
+        }
+    }
+
+    public class Thangy : Thingy<string>
+    {
+        private ILog _log;
+
+        public Thangy()
+        {
+            _log = LogManager.GetLogger(typeof (Thangy));
+        }
+
+        public override string Doover(string stuff)
+        {
+            _log.Debug("Thangy");
+
+            return "ho ho";
+        }
+    }
+}
---
+++ trunk/Log4netPatterns/log4netPatterns/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "log4netPatterns" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "ABB Grain Ltd" )]
+[assembly: AssemblyProduct( "log4netPatterns" )]
+[assembly: AssemblyCopyright( "Copyright © ABB Grain Ltd 2009" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "35e461ea-d9a9-4e75-adc7-4d9247368097" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
