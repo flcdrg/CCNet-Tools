71d8025bc4acc2cd8fce24226f9076a9afc25a66
david@gardiner.net.au <david@gardiner.net.au@0e962e30-8b01-11dd-b073-6769e66693fe>
2009-07-11 06:15:07 +0000

    NCoverDora

---
+++ trunk/NCoverDora/NCoverDora/CoverageExclusion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NCoverDora
+{
+    [XmlRoot()]
+    public class CoverageExclusion
+    {
+        [XmlElement(ElementName="ExclusionType")]
+        public ExclusionType ExclusionType { get; set; }
+
+        [XmlElement()]
+        public string Pattern { get; set; }
+
+        [XmlElement()]
+        public bool IsRegex { get; set; }
+
+        [XmlElement()]
+        public bool Enabled { get; set; }
+
+    }
+
+    public enum ExclusionType
+    {
+        Assembly,
+
+        Namespace,
+
+        Class
+    }
+}
---
+++ trunk/NCoverDora/NCoverDora/SomeClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCoverDora
+{
+    public class SomeClass
+    {
+
+        public void Method1()
+        {
+            int i = 1;
+            i++;
+        }
+
+        public int Method2(string s)
+        {
+            return int.Parse(s);
+        }
+
+    }
+}
